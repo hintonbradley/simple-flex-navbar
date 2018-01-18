bb47a8753742aa948f0292e3829cb116f8f36784
James Bradley <hintonbradley@gmail.com>
2018-01-17 19:33:25 -0800

    updated mobile view

--- public/app.js
+++ public/app.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
 	$('.menu').click(function(){
 		$('.links-wrapper').toggleClass('active');
 	});
+	$('section').click(function(){
+		console.log('i was clicked');
+		if($( ".links-wrapper" ).hasClass( "active" )) {
+			console.log('true');
+			$('.links-wrapper').removeClass('active')
+		}
+	});
 
 })
